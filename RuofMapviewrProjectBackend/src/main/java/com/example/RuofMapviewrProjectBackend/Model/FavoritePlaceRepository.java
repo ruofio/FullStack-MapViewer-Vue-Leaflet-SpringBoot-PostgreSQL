027653fc7b39dd4488134f94554de9850b6137e1
RuofFahad <118277115+ruofio@users.noreply.github.com>
2025-04-14 21:07:31 +0300

    Delete RuofMapviewrProjectBackend directory

--- RuofMapviewrProjectBackend/src/main/java/com/example/RuofMapviewrProjectBackend/Model/FavoritePlaceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.RuofMapviewrProjectBackend.Model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface FavoritePlaceRepository extends JpaRepository<FavoritePlace, Long> {
-    List<FavoritePlace> findByUser(MyAppUser user);
-
-}
