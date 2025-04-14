68089eefac316dfe6412be7be02147ccced3bd0e
RuofFahad <118277115+ruofio@users.noreply.github.com>
2025-04-14 15:57:29 +0300

    Update main.js

--- RuofMapviewerProjectFrontend/src/main.js
+++ RuofMapviewerProjectFrontend/src/main.js
@@ -7,7 +7,9 @@ import 'vuetify/styles'
 import * as components from 'vuetify/components' 
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:8080/api';
 const vuetify = createVuetify({
   components,
   directives,
