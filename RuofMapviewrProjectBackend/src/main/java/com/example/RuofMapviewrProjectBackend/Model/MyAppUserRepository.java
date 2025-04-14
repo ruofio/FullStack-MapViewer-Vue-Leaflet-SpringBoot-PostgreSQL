c8ddbd61f59db7c5638e4bb53e84d021285854ab
RuofFahad <118277115+ruofio@users.noreply.github.com>
2025-04-14 19:24:11 +0300

    Delete RuofMapviewrProjectBackend directory

--- RuofMapviewrProjectBackend/src/main/java/com/example/RuofMapviewrProjectBackend/Model/MyAppUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.RuofMapviewrProjectBackend.Model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface MyAppUserRepository extends JpaRepository<MyAppUser, Long> {
-    Optional<MyAppUser> findByUsername(String username);
-    Optional<MyAppUser> findByEmail(String email);
-}
--- RuofMapviewrProjectBackend/src/main/java/com/example/RuofMapviewrProjectBackend/Model/UserDataSourceConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.RuofMapviewrProjectBackend.Model;
-
-public class UserDataSourceConfig {
-    
-}
